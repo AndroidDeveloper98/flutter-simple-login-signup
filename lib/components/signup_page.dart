924b54386a32b61fb24293556468ab60a3b3fe87
sumit-android-dev <84369514+sumit-android-dev@users.noreply.github.com>
2023-04-08 19:51:15 +0530

    Add next button in keyboard emitaction

--- lib/components/signup_page.dart
+++ lib/components/signup_page.dart
@@ -1,14 +1,13 @@
 import 'dart:io';
+
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:login_signup/components/common/page_header.dart';
-import 'package:login_signup/components/common/page_heading.dart';
-import 'package:login_signup/components/login_page.dart';
-
 import 'package:login_signup/components/common/custom_form_button.dart';
 import 'package:login_signup/components/common/custom_input_field.dart';
+import 'package:login_signup/components/common/page_heading.dart';
+import 'package:login_signup/components/login_page.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({Key? key}) : super(key: key);
@@ -18,7 +17,6 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
-
   File? _profileImage;
 
   final _signupFormKey = GlobalKey<FormState>();
@@ -26,7 +24,7 @@ class _SignupPageState extends State<SignupPage> {
   Future _pickProfileImage() async {
     try {
       final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-      if(image == null) return;
+      if (image == null) return;
 
       final imageTemporary = File(image.path);
       setState(() => _profileImage = imageTemporary);
@@ -47,17 +45,23 @@ class _SignupPageState extends State<SignupPage> {
                 //const PageHeader(),
                 Container(
                   decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.vertical(top: Radius.circular(20),),
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
                   ),
                   child: Column(
                     children: [
-                      const PageHeading(title: 'Sign-up',),
+                      const PageHeading(
+                        title: 'Sign-up',
+                      ),
                       SizedBox(
                         width: 130,
                         height: 130,
                         child: CircleAvatar(
                           backgroundColor: Colors.grey.shade200,
-                          backgroundImage: _profileImage != null ? FileImage(_profileImage!) : null,
+                          backgroundImage: _profileImage != null
+                              ? FileImage(_profileImage!)
+                              : null,
                           child: Stack(
                             children: [
                               Positioned(
@@ -70,7 +74,8 @@ class _SignupPageState extends State<SignupPage> {
                                     width: 50,
                                     decoration: BoxDecoration(
                                       color: Colors.blue.shade400,
-                                      border: Border.all(color: Colors.white, width: 3),
+                                      border: Border.all(
+                                          color: Colors.white, width: 3),
                                       borderRadius: BorderRadius.circular(25),
                                     ),
                                     child: const Icon(
@@ -85,81 +90,111 @@ class _SignupPageState extends State<SignupPage> {
                           ),
                         ),
                       ),
-                      const SizedBox(height: 16,),
+                      const SizedBox(
+                        height: 16,
+                      ),
                       CustomInputField(
                           labelText: 'Name',
                           hintText: 'Your name',
                           isDense: true,
                           validator: (textValue) {
-                            if(textValue == null || textValue.isEmpty) {
+                            if (textValue == null || textValue.isEmpty) {
                               return 'Name field is required!';
                             }
-                            if(textValue.length<5){
+                            if (textValue.length < 5) {
                               return "Enter valid Name";
                             }
                             return null;
-                          }
+                          }),
+                      const SizedBox(
+                        height: 16,
                       ),
-                      const SizedBox(height: 16,),
                       CustomInputField(
                           labelText: 'Email',
                           hintText: 'Your email id',
                           isDense: true,
                           validator: (textValue) {
-                            if(textValue == null || textValue.isEmpty) {
+                            if (textValue == null || textValue.isEmpty) {
                               return 'Email is required!';
                             }
-                            if(!EmailValidator.validate(textValue)) {
+                            if (!EmailValidator.validate(textValue)) {
                               return 'Please enter a valid email';
                             }
                             return null;
-                          }
+                          }),
+                      const SizedBox(
+                        height: 16,
                       ),
-                      const SizedBox(height: 16,),
                       CustomInputField(
                           labelText: 'Contact no.',
                           hintText: 'Your contact number',
                           isDense: true,
                           validator: (textValue) {
-                            if(textValue == null || textValue.isEmpty) {
+                            if (textValue == null || textValue.isEmpty) {
                               return 'Contact number is required!';
                             }
                             return null;
-                          }
+                          }),
+                      const SizedBox(
+                        height: 16,
                       ),
-                      const SizedBox(height: 16,),
                       CustomInputField(
                         labelText: 'Password',
                         hintText: 'Your password',
                         isDense: true,
                         obscureText: true,
                         validator: (textValue) {
-                          if(textValue == null || textValue.isEmpty) {
+                          if (textValue == null || textValue.isEmpty) {
                             return 'Password is required!';
                           }
                           return null;
                         },
                         suffixIcon: true,
                       ),
-                      const SizedBox(height: 22,),
-                      CustomFormButton(innerText: 'Signup', onPressed: _handleSignupUser,),
-                      const SizedBox(height: 18,),
+                      const SizedBox(
+                        height: 22,
+                      ),
+                      CustomFormButton(
+                        innerText: 'Signup',
+                        onPressed: _handleSignupUser,
+                      ),
+                      const SizedBox(
+                        height: 18,
+                      ),
                       SizedBox(
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
-                            const Text('Already have an account ? ', style: TextStyle(fontSize: 13, color: Color(0xff939393), fontWeight: FontWeight.bold),),
+                            const Text(
+                              'Already have an account ? ',
+                              style: TextStyle(
+                                  fontSize: 13,
+                                  color: Color(0xff939393),
+                                  fontWeight: FontWeight.bold),
+                            ),
                             GestureDetector(
                               onTap: () => {
-                                Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage()))
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const LoginPage()))
                               },
-                              child: const Text('Log-in', style: TextStyle(fontSize: 15, color: Color(0xff748288), fontWeight: FontWeight.bold),),
+                              child: const Text(
+                                'Log-in',
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Color(0xff748288),
+                                    fontWeight: FontWeight.bold),
+                              ),
                             ),
                           ],
                         ),
                       ),
-                      const SizedBox(height: 30,),
+                      const SizedBox(
+                        height: 30,
+                      ),
                     ],
                   ),
                 ),
