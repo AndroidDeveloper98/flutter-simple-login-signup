da9276881be199984b9b9c31211e4506efad28d6
sumit-android-dev <84369514+sumit-android-dev@users.noreply.github.com>
2023-04-10 15:58:45 +0530

    Add splash screen

---
+++ lib/components/splash_screen.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:login_signup/components/login_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+  final Color backgroundColor = Colors.white;
+  final TextStyle styleTextUnderTheLoader = const TextStyle(
+      fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.black);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  final splashDelay = 5;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadWidget();
+  }
+
+  _loadWidget() async {
+    var duration = Duration(seconds: splashDelay);
+    return Timer(duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (BuildContext context) => const LoginPage()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Image.asset(
+          'assets/images/flutter_icon.png',
+          width: 200.0,
+          height: 100.0,
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:login_signup/components/login_page.dart';
+import 'package:login_signup/components/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: LoginPage(),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
     );
   }
 }
-
